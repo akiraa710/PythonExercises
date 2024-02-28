f5d0ba57e299af6258b0b071d07b6be889f1c781
akiraa710 <21211@students.mrgs,school.nz>
2024-02-28 14:42:17 +1300

    initial commit

---
+++ GUI/Button-Clicer/Button-click.py
@@ -0,0 +1,42 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from tkinter import *
+>>> 
+>>> def btn_click():
+...     print("Button Clicked")
+... 
+...     
+>>> window = Tk()
+>>> btn = Button(window, text="Click me!", command=btn_click)
+>>> 
+>>> btn.pack()
+>>> 
+>>> window.mainloop()
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
+Button Clicked
